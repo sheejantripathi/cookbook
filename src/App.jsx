48e1d052b6a06350f20af37bbca4113e5456b8ec
Sheejan Tripathi <sheejantripathi101@gmail.com>
2024-06-14 22:48:47 +0200

    gooogle auth login, recipe page and navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,14 @@
-// import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
-// import RecipesList from "./components/RecipesList";
-
-import React, { useState, useEffect } from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import RecipesList from "./components/RecipesList";
+import { useState, useEffect } from "react";
 import { googleLogout, useGoogleLogin } from "@react-oauth/google";
 import axios from "axios";
+import LoginPage from "./components/LoginPage";
+import CustomNavbar from "./components/Navbar";
 
 function App() {
-  // return (
-  //   <>
-  //     {/* <div className="container">
-  //       <h1 className="mt-5 mb-5 text-center">
-  //         <b>The Ultimate CookBook of Champions</b>
-  //       </h1>
-  //       <BrowserRouter>
-  //         <Routes>
-  //           <Route path="/" element={<RecipesList />} />
-  //         </Routes>
-  //       </BrowserRouter>
-  //     </div> */}
-  //   </>
-  // );
-  const [user, setUser] = useState([]);
-  const [profile, setProfile] = useState([]);
+  const [user, setUser] = useState(null); // Changed from [] to null
+  const [profile, setProfile] = useState(null); // Changed from [] to null
 
   const login = useGoogleLogin({
     onSuccess: (response) => setUser(response),
@@ -54,24 +41,26 @@ function App() {
   };
 
   return (
-    <div>
-      <h2>React Google Login</h2>
-      <br />
-      <br />
-      {profile ? (
-        <div>
-          <img src={profile.picture} alt="user image" />
-          <h3>User Logged in</h3>
-          <p>Name: {profile.name}</p>
-          <p>Email Address: {profile.email}</p>
-          <br />
-          <br />
-          <button onClick={logOut}>Log out</button>
-        </div>
-      ) : (
-        <button onClick={login}>Sign in with Google 🚀 </button>
-      )}
-    </div>
+    <Router>
+      <div>
+        {profile ? (
+          <>
+            <CustomNavbar profile={profile} logOut={logOut} />
+            <div className="container">
+              <h1 className="mt-5 mb-5 text-center">
+                <b>The Ultimate CookBook of Champions</b>
+              </h1>
+              <Routes>
+                <Route path="/" element={<RecipesList />} />
+              </Routes>
+            </div>
+          </>
+        ) : (
+          <LoginPage login={login} />
+        )}
+      </div>
+    </Router>
   );
 }
+
 export default App;
--- src/components/RecipesList.jsx
+++ src/components/RecipesList.jsx
@@ -1,5 +1,4 @@
 import React, { useState, useEffect } from "react";
-import axios from "axios";
 import { Container, Row, Col, Card, Pagination } from "react-bootstrap";
 
 const dummyData = [
